529e2c7edd64376470de88638fc2bee52cc7f052
Oscaz <oscaz45@gmail.com>
2021-02-24 21:15:28 +1100

    fix priority typo

--- src/test/java/me/quantiom/eventhandler/PriorityTest.java
+++ src/test/java/me/quantiom/eventhandler/PriorityTest.java
@@ -6,17 +6,17 @@ import me.quantiom.eventhandler.event.impl.TestEvent;
 public class PriorityTest implements EventListener {
     private String data = "None";
 
-    @HandleEvent(priorty = EventPriority.LOWEST)
+    @HandleEvent(priority = EventPriority.LOWEST)
     public void onTestEventLowest(TestEvent event) {
         this.data = "Lowest priority called";
     }
 
-    @HandleEvent(priorty = EventPriority.HIGHEST)
+    @HandleEvent(priority = EventPriority.HIGHEST)
     public void onTestEventHighest(TestEvent event) {
         this.data = "Highest priority called";
     }
 
-    @HandleEvent(priorty = EventPriority.NORMAL)
+    @HandleEvent(priority = EventPriority.NORMAL)
     public void onTestEventNormal(TestEvent event) {
         this.data = "Normal priority called";
     }
